523854e8b23747ee4f719c496b72e3f048860fa3
Patsagorn <patsagorn_y@outlook.com>
2025-03-09 00:34:30 +0700

    feat: add axois api interface

---
+++ src/lib/client/api.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { app } from '$lib/constants'
+
+const axiosInstance = axios.create({
+  baseURL: app.apiBaseUrl,
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export {
+  axiosInstance as api,
+}
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,5 @@
 export const app = {
   name: "CU Openhouse 2025 Refine",
   description: "A project to refine the CU Openhouse 2025 project's data",
+  apiBaseUrl: "http://localhost:3000",
 }
