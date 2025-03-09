5125bb9e13b4e02ce50b0242074e0513ca2069ac
Patsagorn <patsagorn_y@outlook.com>
2025-03-09 18:56:32 +0700

    feat(layout): add dynamic title and meta tags using app constants

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
+	import { app } from '$lib/constants';
 	import '../app.css';
 	let { children } = $props();
 </script>
+
+<svelte:head>
+	<title>
+		{app.name}
+	</title>
+	<meta name="description" content={app.description} />
+	<meta name="og:title" content={app.name} />
+	<meta name="og:description" content={app.description} />
+</svelte:head>
 
 {@render children()}
