f3a0afa5c62a11aae47287ff52ce749d090680cb
Patsagorn <patsagorn_y@outlook.com>
2025-03-19 00:51:18 +0700

    feat: add tab and tab-content components for improved UI organization

---
+++ src/lib/components/tabs/tab-content.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { getContext } from 'svelte';
+
+	const {
+		value,
+		label,
+		children,
+		selected,
+		...props
+	}: {
+		value?: string;
+		label?: string;
+		children?: any;
+		selected?: boolean;
+	} = $props();
+
+	const tabUid = getContext('tabs-group-name');
+</script>
+
+<label class="tab">
+	<input type="radio" {value} name={`tabs-${tabUid}`} checked={selected} />
+	{label}
+</label>
+<div class="tab-content bg-base-100 mt-1 p-6">
+	{@render children?.()}
+</div>
---
+++ src/lib/components/tabs/tabs.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { setContext } from 'svelte';
+
+	const { children, ...props } = $props();
+	const uid = $props.id();
+
+	setContext('tabs-group-name', uid);
+</script>
+
+<div
+	class="tabs tabs-box my-5 w-full [&>.tab-content]:rounded-sm"
+	style="--vis-xy-label-fill-color: transparent;"
+	{...props}
+>
+	{@render children?.()}
+</div>
