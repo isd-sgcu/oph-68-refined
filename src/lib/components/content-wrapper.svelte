943330e5674c38ebd1d9aaed3d0f7453657d4a64
Patsagorn <patsagorn_y@outlook.com>
2025-03-19 00:58:19 +0700

    refactor: remove unused imports and refetch button from content wrapper component

--- src/lib/components/content-wrapper.svelte
+++ src/lib/components/content-wrapper.svelte
@@ -1,11 +1,7 @@
 <script lang="ts">
-	import AppSidebarToggle from './app-sidebar-toggle.svelte';
 	import { app } from '$lib/constants';
-	import PanelLeftOpen from 'lucide-svelte/icons/panel-left-open';
-	import { getContext } from 'svelte';
 	import type { Snippet } from 'svelte';
 	import { getQueryClientContext } from '@tanstack/svelte-query';
-	import RotateCw from 'lucide-svelte/icons/rotate-cw';
 
 	const {
 		children,
@@ -14,7 +10,6 @@
 		children: Snippet;
 		breadcrumbs?: Snippet;
 	} = $props();
-	const queryClient = getQueryClientContext();
 </script>
 
 <div class="navbar bg-base-100 min-h-14 justify-between shadow-sm">
@@ -35,10 +30,6 @@
 	<a href="https://www.cuopenhouse2025.com/" class="btn btn-ghost">
 		{app.name}
 	</a>
-	<button onclick={() => queryClient.refetchQueries()} class="btn btn-ghost">
-		<RotateCw class="size-4 shrink-0" />
-		<span class="sr-only">โหลดข้อมูลใหม่</span>
-	</button>
 </div>
 <main
 	class="prose dark:prose-invert prose-neutral prose-p:max-w-prose m-2 mx-auto max-w-5xl rounded-2xl p-8"
